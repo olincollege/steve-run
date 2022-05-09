74a0fd815e64203692f7fa390e7b85b37f58010f
Lily Jiang <ljiang@olin.edu>
2022-05-08 23:49:12 -0400

    add docstrings to steve_controller.py

--- steve_controller.py
+++ steve_controller.py
@@ -1,15 +1,53 @@
-# determines what steve does in response to keyboard inputs and touching obstacles. Sends that data to the model (ex: space bar is pressed --> this controller tells the model that steve is 'jumping' right now --> model state (the 'jumping' variable) is read by view to update steve's view --> after a period of time steve is not jumping anymore (either make this time based or y-level based) --> controller updates this to the model --> view reads this from model and updates steve's view)
+'''
+Controller for the character in the game, Steve.
+
+Takes in keyboard inputs and moves Steve based upon it.
+'''
 
 import pygame
 from steve_run import SteveRun
 
+
 class SteveController:
+    '''
+    Class representing a controller for the player/character in the game.
+
+    Attributes:
+        steve_run: an instance of the SteveRun class, representing the current
+            instance of the game.
+        duck_image: an array of 2 images representing Steve when ducking to
+            avoid flying obstacles. Alternating between these images simulates
+            a running animation.
+        run_image: an array of 2 images representing Steve when running
+            normally.  Alternating between these images simulates a running
+            animation.
+        jump_image: an image representing Steve when jumping.
+        steve_ducking: a boolean representing whether Steve is currently
+            ducking or not.
+        steve_running = a boolean representing whether Steve is currently
+            running or not.
+        steve_jumping = a boolean representing whether Steve is currently
+            jumping or not.
+        step_index: an integer representing the duration each frame of a
+            running/ducking animation lasts.
+        jump_velocity: a float representing the speed at which Steve is rising
+            or falling over the duration of a jump.
+        steve_rect: an array of 2 integers representing the size of Steve's
+            image.
+        steve_rect.x: an integer representing the x-dimension of Steve's image.
+        steve_rect.y: an integer representing the y-dimension of Steve's image.
+    '''
     X_POSITION = 80
     Y_POSITION = 310
     Y_POSITION_DUCKING = 340
     JUMP_VELOCITY = 8.5
 
     def __init__(self, steve_run):
+        '''
+        Args:
+            steve_run: an instance of the SteveRun class, representing the
+                current game being played.
+        '''
         self.steve_run = steve_run
 
         self.duck_image = SteveRun.DUCKING_IMAGE
@@ -21,13 +59,18 @@ class SteveController:
         self.steve_jumping = False
 
         self.step_index = 0
-        self.jump_vel = self.JUMP_VELOCITY
-        # self.image = self.run_image[0]
+        self.jump_velocity = self.JUMP_VELOCITY
         self.steve_rect = self.steve_run.image().get_rect()
         self.steve_rect.x = self.X_POSITION
         self.steve_rect.y = self.Y_POSITION
 
     def update(self, user_input):
+        '''
+        Changes Steve's state based on keyboard input and jumping status.
+
+        Args:
+            user_input: the input read in from the player's keyboard.
+        '''
         if self.steve_ducking:
             self.duck()
         if self.steve_running:
@@ -38,20 +81,23 @@ class SteveController:
         if self.step_index >= 10:
             self.step_index = 0
 
-        if user_input[pygame.K_UP] and not self.steve_jumping:
+        if user_input[pygame.K_SPACE] and not self.steve_jumping:
             self.steve_ducking = False
             self.steve_running = False
             self.steve_jumping = True
-        elif user_input[pygame.K_DOWN] and not self.steve_jumping:
+        elif user_input[pygame.K_LSHIFT] and not self.steve_jumping:
             self.steve_ducking = True
             self.steve_running = False
             self.steve_jumping = False
-        elif not (self.steve_jumping or user_input[pygame.K_DOWN]):
+        elif not (self.steve_jumping or user_input[pygame.K_LSHIFT]):
             self.steve_ducking = False
             self.steve_running = True
             self.steve_jumping = False
 
     def duck(self):
+        '''
+        Changes Steve's state to be 'ducking', and updates the model.
+        '''
         self.image = self.duck_image[self.step_index // 5]
         self.steve_run.update_image(self.image)
         self.steve_rect = self.image.get_rect()
@@ -60,6 +106,9 @@ class SteveController:
         self.step_index += 1
 
     def run(self):
+        '''
+        Changes Steve's state to be 'running', and updates the model.
+        '''
         self.image = self.run_image[self.step_index // 5]
         self.steve_run.update_image(self.image)
         self.steve_rect = self.image.get_rect()
@@ -68,11 +117,14 @@ class SteveController:
         self.step_index += 1
 
     def jump(self):
+        '''
+        Changes Steve's state to be 'jumping', and updates the model.
+        '''
         self.image = self.jump_image
         self.steve_run.update_image(self.image)
         if self.steve_jumping:
-            self.steve_rect.y -= self.jump_vel * 4
-            self.jump_vel -= 0.8
-        if self.jump_vel < -self.JUMP_VELOCITY:
+            self.steve_rect.y -= self.jump_velocity * 4
+            self.jump_velocity -= 0.8
+        if self.jump_velocity < -self.JUMP_VELOCITY:
             self.steve_jumping = False
-            self.jump_vel = self.JUMP_VELOCITY
+            self.jump_velocity = self.JUMP_VELOCITY
