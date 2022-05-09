c5cfa4b872a77494a8c90c341278f6f770fde1ee
Lily Jiang <ljiang@olin.edu>
2022-05-08 23:27:51 -0400

    remove unnecessary files

--- obstacle_controller.py
+++
@@ -1,9 +0,0 @@
-class ObstacleController:
-    def __init__(self, steve_run):
-        '''
-        Args:
-        image: __
-        type: ___
-        '''
-        self._steve_run = steve_run
-        self._obstacles = self._steve_run.obstacles()
