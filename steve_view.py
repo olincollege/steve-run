e175891ace9a122348c3b43b447dcc5b418fd332
Lily Jiang <ljiang@olin.edu>
2022-05-09 00:27:35 -0400

    add docstrings to steve_view.py

--- steve_view.py
+++ steve_view.py
@@ -1,11 +1,30 @@
-# reads in data from the model and displays it
+'''
+View for the Steve character.
+'''
 
 class SteveView:
+    '''
+    The View for the Steve character within the current instance of the Steve
+    Run game.
+
+    Attributes:
+        steve_run: an instance of SteveRun, representing the current game of
+            Steve Run being played.
+        steve_controller: an instance of the SteveController class.
+    '''
     def __init__(self, steve_run, steve_controller):
+        '''
+        Args:
+            steve_run: an instance of SteveRun, representing the current game of
+                Steve Run being played.
+            steve_controller: an instance of the SteveController class.
+        '''
         self._steve_run = steve_run
         self._steve_controller = steve_controller
 
     def draw(self, screen):
-        print(self._steve_run.image())
+        '''
+        Display the Steve character in its current form.
+        '''
         screen.blit(self._steve_run.image(), (self._steve_controller.steve_rect.x,
                                  self._steve_controller.steve_rect.y))
